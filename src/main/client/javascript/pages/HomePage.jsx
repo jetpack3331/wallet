02e673d387f15fe4a34f2ec19acba6457d65ae82
Matt Byrne <matt@3wks.com.au>
2018-03-06 19:59:26 +1100

    Ryan: Initial stying of the pages. Adding for @farrowism until he gets access.

--- src/main/client/javascript/pages/HomePage.jsx
+++ src/main/client/javascript/pages/HomePage.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router';
 import './HomePage.less';
+import NoWalletIcon from '../../images/icon-no-wallet.png';
+import GoogleIcon from '../../images/g-logo.png';
 
 const HomePage = () => (
   <div className="home-page">
-    <div>
-      <Link to="/login/google" target="_self">Login</Link>
+    <div className="container">
+      <div className="login-container">
+        <div className="main-icon"><img className="logo" src={NoWalletIcon} alt="HUT34 Wallet"/></div>
+        <h1 className="display-1"><strong>Sign in to create a wallet</strong></h1>
+
+
+        <p>Sign in using your Google account.</p>
+        <Link class="google-login" to="/login/google" target="_self"><img className="google-icon" src={GoogleIcon} alt="G"/> Sign in using Google</Link>
+      </div>
     </div>
+    <footer><p className="footer-disclaimer">2018&copy; Hut34 Wallet | The Open Source Crypto Wallet</p></footer>
   </div>
 );
 
--- src/main/client/javascript/pages/WalletPage.jsx
+++ src/main/client/javascript/pages/WalletPage.jsx
@@ -6,31 +6,42 @@ import { createWalletAccount } from '../actions/wallets';
 import './HomePage.less';
 import { walletAccount } from '../model';
 import { getFirstWalletAccount } from '../reducers';
+import NoWalletIcon from '../../images/icon-no-wallet.png';
 
 const WalletPage = props => (
   <div className="wallet-page">
-    <h1 className="display-2">My Wallet</h1>
-    <div className="widgets">
-      <div>
-        {props.walletAccount &&
-        <div>Wallet created: {props.walletAccount.address}</div>
-        }
+    <div className="container">
+      <div className="widgets">
+        <div>
+          {props.walletAccount &&
+          <div className="row wallets">
+            <h1 className="display-1"><strong>My Wallet</strong></h1>
+            <p>Wallet created: {props.walletAccount.address}</p>
+          </div>
+          }
 
-        {!props.walletAccount &&
+          {!props.walletAccount &&
+          <div className="row no-wallet">
+            <div className="main-icon"><img className="icon no-wallet" src={NoWalletIcon} alt="No Wallet"/></div>
+            <h1 className="display-1"><strong>No Wallet</strong></h1>
+            <p>Add one now</p>
+            <Button
+              className="btn-primary"
+              variant="raised"
+              onClick={() => props.createWalletAccount('somepassword123')}
+            >
+              Create Wallet Account
+            </Button>
+          </div>
+          }
 
-        <Button
-          variant="raised"
-          onClick={() => props.createWalletAccount('somepassword123')}
-        >
-          Create Wallet Account
-        </Button>
 
-        }
-
-
+        </div>
       </div>
     </div>
+    <footer><p className="footer-disclaimer">2018&copy; Hut34 Wallet | The Open Source Crypto Wallet</p></footer>
   </div>
+
 );
 
 WalletPage.propTypes = {

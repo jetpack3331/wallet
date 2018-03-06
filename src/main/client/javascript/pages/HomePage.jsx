baa89d9a6609fe8e7e8b2b609f0b469c867677be
Martin Levett <martin@3wks.com.au>
2018-03-06 17:41:45 +1100

    Add site appbar to layout

--- src/main/client/javascript/pages/HomePage.jsx
+++ src/main/client/javascript/pages/HomePage.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import { Link } from 'react-router';
 import './HomePage.less';
 
-
 const HomePage = () => (
   <div className="home-page">
-    <h1 className="display-2">Hut34 Wallet</h1>
-    <div className="widgets">
-      <div>
-        <Link to="/login/google" target="_self">Login</Link>
-      </div>
+    <div>
+      <Link to="/login/google" target="_self">Login</Link>
     </div>
   </div>
 );
--- src/main/client/javascript/pages/Layout.jsx
+++ src/main/client/javascript/pages/Layout.jsx
@@ -1,17 +1,73 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { AppBar, IconButton, Menu, MenuItem, Toolbar } from 'material-ui';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import { connect } from 'react-redux';
+import './Layout.less';
+import { getLoggedInUser } from '../reducers';
 
-const Layout = ({ children }) => (
-  <div className="default-layout">
-    { children }
+const open = false;
+const logoImage = require('../../images/logo-hut34-wallet.png');
+
+const Layout = ({ user, children }) => (
+  <div className="layout">
+    <AppBar className="app-bar" position="static" color="inherit">
+      <Toolbar>
+        <div className="title-container">
+          <img className="logo" src={logoImage} alt="HUT34 Wallet"/>
+        </div>
+        {user && user.name && (
+          <div>
+            <IconButton
+              aria-owns={open ? 'menu-appbar' : null}
+              aria-haspopup="true"
+              onClick={this.handleMenu}
+              color="inherit"
+            >
+              <AccountCircle/>
+            </IconButton>
+            <span>{user.name}</span>
+            <Menu
+              id="menu-appbar"
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={this.handleClose}
+            >
+              <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+            </Menu>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+    <div className="default-layout">
+      {children}
+    </div>
   </div>
 );
 
 Layout.propTypes = {
+  user: PropTypes.object,
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]).isRequired,
 };
 
-export default Layout;
+Layout.defaultProps = {
+  user: null,
+};
+
+const mapStateToProps = state => ({
+  user: getLoggedInUser(state),
+});
+
+const actions = {};
+
+export default connect(mapStateToProps, actions)(Layout);

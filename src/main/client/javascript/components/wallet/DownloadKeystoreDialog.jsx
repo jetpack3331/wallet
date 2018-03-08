cbbf84df7cc4899ca048c1e50fd525bcf65428c5
Martin Levett <martin@3wks.com.au>
2018-03-09 09:36:25 +1100

    Add dialog prompt when downloading keystore

---
+++ src/main/client/javascript/components/wallet/DownloadKeystoreDialog.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from 'material-ui';
+import FileDownload from 'material-ui-icons/FileDownload';
+
+const DownloadKeystoreDialog = ({ open, onClose, walletAccount }) => (
+  <Dialog
+    open={open}
+    onClose={onClose}
+    fullWidth
+    maxWidth="sm"
+  >
+    <DialogTitle>
+      Download keystore
+    </DialogTitle>
+    <DialogContent>
+      <p>
+        Your keystore can be downloaded in standard UTC JSON format used by most Etherium clients.
+        This is encrypted with the password you provided when creating the wallet.
+      </p>
+    </DialogContent>
+    <DialogActions>
+      <Button variant="flat" onTouchTap={onClose}>
+        Cancel
+      </Button>
+      <Button
+        variant="flat"
+        size="small"
+        href={`/api/wallets/accounts/${walletAccount.address}/download`}
+        onTouchTap={onClose}
+        fullWidth
+      >
+        <FileDownload className="btn-icon-left"/>
+        Download
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+DownloadKeystoreDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  walletAccount: PropTypes.object.isRequired,
+};
+
+export default DownloadKeystoreDialog;
+

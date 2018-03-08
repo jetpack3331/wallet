c52971853944eda3b38608ee7d6fda43c567cc00
Martin Levett <martin@3wks.com.au>
2018-03-09 08:59:11 +1100

    Remove button tooltips to fix layout after device orientation change

--- src/main/client/javascript/components/wallet/DownloadKeystoreButton.jsx
+++ src/main/client/javascript/components/wallet/DownloadKeystoreButton.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Tooltip } from 'material-ui';
+import { Button } from 'material-ui';
 import LockOutline from 'material-ui-icons/LockOutline';
 
 const DownloadKeystoreButton = ({ walletAccount }) => (
-  <Tooltip title="Download encrypted keystore" enterDelay={300}>
-    <Button
-      className="btn-primary"
-      variant="raised"
-      size="small"
-      href={`/api/wallets/accounts/${walletAccount.address}/download`}
-      fullWidth
-    >
-      <LockOutline className="btn-icon-left"/>
-      Keystore
-    </Button>
-  </Tooltip>
+  <Button
+    className="btn-primary"
+    variant="raised"
+    size="small"
+    href={`/api/wallets/accounts/${walletAccount.address}/download`}
+    fullWidth
+  >
+    <LockOutline className="btn-icon-left"/>
+    Keystore
+  </Button>
 );
 
 DownloadKeystoreButton.propTypes = {
--- src/main/client/javascript/components/wallet/ViewPrivateKeyButton.jsx
+++ src/main/client/javascript/components/wallet/ViewPrivateKeyButton.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Tooltip } from 'material-ui';
+import { Button } from 'material-ui';
 import VpnKey from 'material-ui-icons/VpnKey';
 import ViewPrivateKeyDialog from './ViewPrivateKeyDialog';
 
@@ -20,18 +20,16 @@ class ViewPrivateKeyButton extends React.Component {
   render() {
     return (
       <Fragment>
-        <Tooltip title="View private key" enterDelay={300}>
-          <Button
-            className="btn-primary btn-view-private-key"
-            variant="raised"
-            size="small"
-            onClick={this.openDialog}
-            fullWidth
-          >
-            <VpnKey className="btn-icon-left"/>
-            Key
-          </Button>
-        </Tooltip>
+        <Button
+          className="btn-primary btn-view-private-key"
+          variant="raised"
+          size="small"
+          onClick={this.openDialog}
+          fullWidth
+        >
+          <VpnKey className="btn-icon-left"/>
+          Key
+        </Button>
         <ViewPrivateKeyDialog
           walletAccount={this.props.walletAccount}
           open={this.state.dialogOpen}

bb7ec2bb8f55496705420693e39c44a2eca6393d
Martin Levett <martin@3wks.com.au>
2018-03-23 14:49:43 +1100

    Split text into paragraphs

--- src/main/client/javascript/pages/FirstLoginPage.jsx
+++ src/main/client/javascript/pages/FirstLoginPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Grid } from 'material-ui';
+import { Link } from 'react-router';
 import AcceptTermsForm from '../components/forms/AcceptTermsForm';
 import { acceptTerms, logoutToHome } from '../actions/auth';
 import './FirstLoginPage.less';
@@ -28,15 +29,22 @@ const FirstLoginPage = ({ onAcceptTerms, onRejectTerms }) => (
                   Safety and Security
                 </div>
                 <div className="info-description">
-                  It&apos;s important you understand the nature of the Hut34 Wallet,
-                  in order to make the best safety and security decisions for
-                  you as an individual.
-                  All addresses you create within the wallet are encrypted,
-                  and stored in the Google Cloud Platform as associated with
-                  your Google account. We utilise enterprise grade security
-                  in the handling of all data, and you, and only you, are able
-                  to decrypt the keys to each address. We talk about this a lot,
-                  get in touch to learn more.
+                  <p>
+                    It&apos;s important you understand the nature of the Hut34 Wallet,
+                    in order to make the best safety and security decisions for
+                    you as an individual.
+                  </p>
+                  <p>
+                    All addresses you create within the wallet are encrypted,
+                    and stored in the Google Cloud Platform as associated with
+                    your Google account. We utilise enterprise grade security
+                    in the handling of all data, and you, and only you, are able
+                    to decrypt the keys to each address.
+                  </p>
+                  <p>
+                    We talk about this a lot,
+                    get in touch to learn more.
+                  </p>
                 </div>
               </div>
             </Grid>
@@ -51,13 +59,20 @@ const FirstLoginPage = ({ onAcceptTerms, onRejectTerms }) => (
                   You are responsible
                 </div>
                 <div className="info-description">
-                  The security of addresses you create within a Hut34 Wallet is linked
-                  to the security of your Google account. We strongly recommend that you
-                  follow the good advice at https://myaccount.google.com/security, and
-                  setup &apos;2FA&apos;, and other security measures to protect your account.
-
-                  Google will also prompt you regularly to complete a security checklist,
-                  which is a powerful tool to ensure your security remains strong, and up to date.
+                  <p>The security of addresses you create within a Hut34 Wallet is linked
+                    to the security of your Google account.
+                  </p>
+                  <p>We strongly recommend that you
+                    follow the good advice at
+                    <strong>
+                      <Link href="https://myaccount.google.com/security" target="_blank"> https://myaccount.google.com/security</Link>
+                    </strong>,
+                    and setup &apos;2FA&apos;, and other security measures to protect your account.
+                  </p>
+                  <p>
+                    Google will also prompt you regularly to complete a security checklist,
+                    which is a powerful tool to ensure your security remains strong, and up to date.
+                  </p>
                 </div>
               </div>
             </Grid>
@@ -71,13 +86,16 @@ const FirstLoginPage = ({ onAcceptTerms, onRejectTerms }) => (
                   Protect from Scams
                 </div>
                 <div className="info-description">
-                  Always keep in mind that once you send a transaction to the Ethereum blockchain,
-                  you will not be able to undo it. We support decentralised &apos;trustless&apos;
-                  models wherever possible that avoid you having to trust any third party with your
-                  tokens.
-
-                  The Hut34 Wallet is a tool to make your life a little easier when interacting
-                  with the Ethereum blockchain, but always be thoughtful and careful.
+                  <p>
+                    Always keep in mind that once you send a transaction to the Ethereum blockchain,
+                    you will not be able to undo it. We support decentralised &apos;trustless&apos;
+                    models wherever possible that avoid you having to trust any third party with your
+                    tokens.
+                  </p>
+                  <p>
+                    The Hut34 Wallet is a tool to make your life a little easier when interacting
+                    with the Ethereum blockchain, but always be thoughtful and careful.
+                  </p>
                 </div>
               </div>
             </Grid>
@@ -90,14 +108,17 @@ const FirstLoginPage = ({ onAcceptTerms, onRejectTerms }) => (
                   So what&apos;s the point?
                 </div>
                 <div className="info-description">
-                  Your Hut34 Wallet will allow you to create addresses incredibly quickly and
-                  easily, and be confident that they are stored securely within the
-                  infrastructure of the Google cloud platform.
-
-                  You may wish to download your keystore and private keys to keep offline
-                  backups, or you may wish to transfer larger value amounts to other storage
-                  devices. The point is that it&apos;s up to you. Enjoy being part of the
-                  Ethereum blockchain!
+                  <p>
+                    Your Hut34 Wallet will allow you to create addresses incredibly quickly and
+                    easily, and be confident that they are stored securely within the
+                    infrastructure of the Google cloud platform.
+                  </p>
+                  <p>
+                    You may wish to download your keystore and private keys to keep offline
+                    backups, or you may wish to transfer larger value amounts to other storage
+                    devices. The point is that it&apos;s up to you. Enjoy being part of the
+                    Ethereum blockchain!
+                  </p>
                 </div>
               </div>
             </Grid>
